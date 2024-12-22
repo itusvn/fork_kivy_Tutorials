c2b020684aa56d1036f6513ba9f99f38200c36f9
TanakaPeter <142831498+TanakaPeter@users.noreply.github.com>
2024-12-22 15:31:51 +0200

    Add files via upload

---
+++ Finance Saas Template/widgets/modals.py
@@ -0,0 +1,8 @@
+from kivy.uix.modalview import ModalView
+from kivy.lang import Builder
+from kivymd.uix.filemanager import MDFileManager
+from kivy.properties import StringProperty, ObjectProperty
+from kivymd.uix.pickers import MDModalDatePicker, MDTimePickerDialVertical
+
+Builder.load_string("""
+""")
---
+++ Finance Saas Template/widgets/tables.py
@@ -0,0 +1,7 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.lang import Builder
+
+Builder.load_string("""
+""")
