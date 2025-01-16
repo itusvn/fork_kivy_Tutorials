7a0a6bc3b11832d2c9180ede317c780eebada133
TanakaPeter <142831498+TanakaPeter@users.noreply.github.com>
2025-01-16 13:16:09 +0200

    Delete Finance Saas Template directory

--- Finance Saas Template/main.py
+++
@@ -1,3 +0,0 @@
-from app import MainApp
-
-MainApp().run()
--- Finance Saas Template/widgets/popups.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.modalview import ModalView
-from kivy.properties import ObjectProperty, StringProperty, ListProperty
-from kivy.lang import Builder
-
-Builder.load_string("""
-
-""")
